9495f5077d06404156c937ad83dd1515899bf049
hypengw <hypengwip@gmail.com>
2026-04-12 03:42:21 +0000

    ui ws-port and clang format

--- ui/src/main.cpp
+++ ui/src/main.cpp
@@ -1,10 +1,29 @@
 #include <QGuiApplication>
+#include <QCommandLineParser>
 #include <QtQml/QQmlExtensionPlugin>
 Q_IMPORT_QML_PLUGIN(waywallen_uiPlugin)
 
 import ncrequest;
 
-int main(int argc, char **argv) {
-  ncrequest::global_init();
-  QGuiApplication gui_app(argc, argv);
+int main(int argc, char** argv) {
+    ncrequest::global_init();
+    QGuiApplication gui_app(argc, argv);
+    QCoreApplication::setApplicationName(APP_NAME);
+    QCoreApplication::setApplicationVersion(APP_VERSION);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOption({"ws-port", "WebSocket port of the waywallen daemon.", "port"});
+    parser.process(gui_app);
+
+    quint16 ws_port = 0;
+    if (parser.isSet("ws-port")) {
+        bool ok = false;
+        ws_port = parser.value("ws-port").toUShort(&ok);
+        if (!ok) {
+            qCritical("invalid --ws-port value: %s", qPrintable(parser.value("ws-port")));
+            return 1;
+        }
+    }
 }
